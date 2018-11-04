8e5795f822b725a7884cb7aab26d3f4ee87146b9
Franz X. Mohr <franz.x.mohr@outlook.com>
2018-11-04 14:15:59 +0100

    New commit

--- R/vcov.prais.R
+++
@@ -1,9 +0,0 @@
-#' @export
-vcov.summary.prais <- function(x, ...) {
-  return(x$sigma^2 * x$cov.unscaled)
-}
-
-#' @export
-vcov.prais <- function(x, ...) {
-  vcov.summary.prais(summary.prais(x), ...)
-}
