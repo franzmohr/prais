2d0e841c53117571a9e5ecebf995b266eb7ac67c
Franz X. Mohr <franz.x.mohr@outlook.com>
2019-01-05 21:54:53 +0100

    Update documentation

--- R/summary.prais.R
+++ R/summary.prais.R
@@ -2,26 +2,37 @@
 #'
 #' summary method for class "prais".
 #'
-#' @param object an object of class "prais", usually, a result of a call to \code{\link{prais_winsten}}.
-#' @param x an object of class "summary.prais", usually, a result of a call to \code{\link{summary.prais}}.
+#' @param object an object of class "prais", usually, a result of a call to
+#' \code{\link{prais_winsten}}.
+#' @param x an object of class "summary.prais", usually, a result of a call to
+#' \code{\link{summary.prais}}.
 #' @param digits the number of significant digits to use when printing.
-#' @param signif.stars logical. If \code{TRUE}, 'significance stars' are printed for each coefficient.
+#' @param signif.stars logical. If \code{TRUE}, 'significance stars' are printed
+#' for each coefficient.
 #' @param ... further arguments passed to or from other methods.
 #'
-#' @return \code{summary.prais} returns a list of class "summary.prais", which contains the following components:
+#' @return \code{summary.prais} returns a list of class "summary.prais",
+#' which contains the following components:
 #' \item{call}{the matched call.}
 #' \item{residuals}{the residuals, that is the response minus the fitted values.}
 #' \item{coefficients}{a named vector of coefficients.}
 #' \item{rho}{the values of the AR(1) coefficient \eqn{\rho} from all iterations.}
 #' \item{sigma}{the square root of the estimated variance of the random error.}
-#' \item{df}{degrees of freedom, a 3-vector \emph{(p, n-p, p*)}, the first being the number of non-aliased coefficients, the last being the total number of coefficients.}
+#' \item{df}{degrees of freedom, a 3-vector \emph{(p, n-p, p*)},
+#' the first being the number of non-aliased coefficients, the last being
+#' the total number of coefficients.}
 #' \item{r.squared}{R^2, the 'fraction of variance explained by the model',
 #' \deqn{R^2 = 1 - \dfrac{\sum (y_i - \hat{y}_i)^2}{\sum (y_i - \overline{y})^2)},}
-#' where \eqn{\overline{y}} is the mean of \eqn{y_i} for \eqn{y_i = 1,...N} if there is an intercept and zero otherwise.}
-#' \item{adj.r.squared}{the above \emph{R^2} statistic \emph{'adjusted'}, penalising for higher \emph{p}.}
-#' \item{fstatistic}{(for models including non-intercept terms) a 3-vector with the value of the F-statistic with its numerator and denominator degrees of freedom.}
-#' \item{cov.unscaled}{a \eqn{p x p} matrix of (unscaled) covariances of the \emph{coef[j], j=1, ..., p}.}
-#' \item{dw}{a named 2-vector with the Durbin-Watson statistic of the original linear model and the Prais-Winsten estimator.}
+#' where \eqn{\overline{y}} is the mean of \eqn{y_i} for \eqn{y_i = 1,...N}
+#' if there is an intercept and zero otherwise.}
+#' \item{adj.r.squared}{the above \emph{R^2} statistic \emph{'adjusted'}, penalising
+#' for higher \emph{p}.}
+#' \item{fstatistic}{(for models including non-intercept terms) a 3-vector with the
+#' value of the F-statistic with its numerator and denominator degrees of freedom.}
+#' \item{cov.unscaled}{a \eqn{p x p} matrix of (unscaled) covariances of the
+#' \emph{coef[j], j=1, ..., p}.}
+#' \item{dw}{a named 2-vector with the Durbin-Watson statistic of the original
+#' linear model and the Prais-Winsten estimator.}
 #' \item{index}{a character specifying the ID and time variables.}
 #'
 #' @export
